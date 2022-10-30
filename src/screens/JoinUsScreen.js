536ed675478292a9078e7a8de0a085e829356cdd
johnsen10 <senejohn@hotmail.com>
2022-10-30 14:43:05 -0400

    joinUsScreen improvment

--- src/screens/JoinUsScreen.js
+++ src/screens/JoinUsScreen.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import emailjs from '@emailjs/browser';
 import swal from 'sweetalert';
+import {AiOutlineCaretRight} from "react-icons/ai"
 
 
 import SelectField from "../components/form/SelectField";
@@ -51,20 +52,35 @@ export default function JoinUsScreen(){
 
     return(<>
         <Banner/>
-        <div className="h-screen">  
-        
-          <div className=" flex content-center items-center justify-center m-10">
-            <form className="bg-blue-50 w-96 mt-6 p-4 rounded-lg shadow-lg " onSubmit={handleSubmit}>
-              <div className="flex flex-col space-y-10">
-                  <TextField name="fullname" type="text" value={userInput.fullname} onChange={handleChange} placeholder="votre nom et prenom" />
-                  <TextField name="email" type="email" value={userInput.email} onChange={handleChange} placeholder="votre email"/>
-                  <SelectField name="subject" onChange={handleChange} />
-                  <TextArea name="message" type="text" value={userInput.message} onChange={handleChange} placeholder="votre message svp"/>
+        <div className="h-screen">
+          <div className="grid grid-cols-2">  
+          {/* left grid for text */}
+            <div className="order-1 flex flex-col justify-center items-center px-5">
+              <h1 className="font-bold text-6xl text-gray-500 brush mx-10 my-5">Parlez nous de vos besoins specifiques</h1>
+              <div className="inline-flex items-baseline ">
+                <span ><AiOutlineCaretRight className="text-md text-blue-500" /></span>
+                <p className=" text-gray-700 my-5 font-medium text-2xl">Avez vous trouvé ce que vous cherchez? C'est super! Ecrivez nous pour aller de l'avant avec le produit concerné</p>
               </div>
-              <Button text="Envoyer"/>
-            </form>
+              <div className="inline-flex items-baseline ">
+                <span ><AiOutlineCaretRight className="text-md text-blue-500" /></span>
+                <p className=" text-gray-700 my-5 font-medium text-2xl">Avez vous des questions, ou bien vous n'avez tout simplement pas trouvé ce que vous voulez, c'est pas grave, 
+                  juste remplir le formulaire avec une description de ce que vous recherchez et vos informations de contact et vous nous répondrons sous peu
+                </p>
+              </div>
+            </div>
+            {/* right grid containing form */}
+            <div className="order-2 flex content-center items-center justify-center m-10">
+              <form className="bg-blue-100 w-96 -mt-20 p-4 rounded-lg shadow-lg" onSubmit={handleSubmit}>
+                <div className="flex flex-col space-y-10">
+                    <TextField name="fullname" type="text" value={userInput.fullname} onChange={handleChange} placeholder="votre nom et prenom" />
+                    <TextField name="email" type="email" value={userInput.email} onChange={handleChange} placeholder="votre email"/>
+                    <SelectField name="subject" onChange={handleChange} />
+                    <TextArea name="message" type="text" value={userInput.message} onChange={handleChange} placeholder="votre message svp"/>
+                </div>
+                <Button text="Envoyer"/>
+              </form>
+            </div>
           </div>
-          {/* <img className="w-96 object-contain banner" src={carBackground} alt="carBackground" /> */}
         </div>
         </>
     )
